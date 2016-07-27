1125a38c8e7724634729ac4269523035e2a485c2
Tom Leskin <tleskin@gmail.com>
2016-07-26 22:02:05 -0400

    Complete lesson 4

---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+let MenuItem = require('./menu-item');
+
+ReactDOM.render( MenuItem(), document.getElementById('app'));
---
+++ src/js/menu-item.js
@@ -0,0 +1,10 @@
+function MenuItem() {
+  return (
+    React.createElement('ul', null, 
+      React.createElement('li', null, 'Menu Item'),
+      React.createElement('li', null, 'Menu Item')
+    )
+  )
+}
+
+module.exports = MenuItem
