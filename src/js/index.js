39c580f88e5c3556667c7bd271b2a3a78f47feef
Tom Leskin <tleskin@gmail.com>
2016-07-26 23:37:40 -0400

    Complete Lesson 7

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,33 @@
 let MenuItem = require('./menu-item');
 
-function Menu (props) {
-  return (
-    <ul>
-      <MenuItem isActive={props.isActive} />
-      <MenuItem />
-    </ul>
-  )
-}
+let Menu = React.createClass({
+  getInitialState(){
+    let subMenuVisible = this.props.isActive ? true : false
+    
+    return {
+      subMenuVisible: subMenuVisible,
+    }
+  },
+  
+  onClick(){
+    this.setState({subMenuVisible: !this.state.subMenuVisible})
+  },
+  
+  render(){
+    let style = {
+      display: this.state.subMenuVisible ? 'none' : 'block'
+    }
+    
+    return (
+      <div>
+        <h1 onClick={this.onClick}>Menu</h1>
+        <ul style={style}>
+          <MenuItem isActive={this.props.isActive} />
+          <MenuItem />
+        </ul>
+      </div>
+    )    
+  }
+});
 
 ReactDOM.render( <Menu isActive={true} />, document.getElementById('app'));
--- src/js/menu-item.js
+++ src/js/menu-item.js
@@ -1,11 +1,39 @@
-function MenuItem({isActive = false}) {
-  let style = {
-    fontWeight: isActive ? 'bold' : 'normal'
-  }
+let MenuItem = React.createClass({
+  getDefaultProps(){
+    return {
+      isActive: false
+    }
+  },
   
-  return (
-    <li style={style}>Menu Item</li>
-  )
-}
+  getInitialState(){
+    let subMenuVisible = this.props.isActive ? true : false
+    
+    return {
+      subMenuVisible: subMenuVisible,
+      isHovering: false
+    }
+  },
+  
+  mouseOver: function () {
+    this.setState({isHovering: true});
+  },
+  
+  mouseOut: function () {
+    this.setState({isHovering: false});
+  },
+  
+  render() {
+    let style = {
+      fontWeight: this.state.subMenuVisible ? 'bold' : 'normal',
+      color: this.state.isHovering ? 'green' : 'black'
+    }
+    
+    return (
+      <li style={style} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
+        Menu Item
+      </li>
+    )
+  }
+});
 
 module.exports = MenuItem
